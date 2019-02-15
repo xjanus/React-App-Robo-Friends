4c19a4fb5eee42387e533d739209ec0dd42cb43e
xjanus <xjanus.official@gmail.com>
2019-02-15 12:44:08 +0200

    compoenentsghf

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const card = () => {
+    return (
+        <div>
+            <img src=''></img>
+            <div>
+                <h2>Name</h2>
+                <p>name@email.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default card;
