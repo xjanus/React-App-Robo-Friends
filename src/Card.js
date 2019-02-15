a6c882dde10ea8fba2557bd6e71938cdc026ae93
xjanus <xjanus.official@gmail.com>
2019-02-15 13:52:02 +0200

    jscript

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const card = (props) => {
+const card = ({name, email, id}) => {
+    //destructuring
+    // const {name, email, id} = props;
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src='https://robohash.org/test?200x200'></img>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}></img>
             <div>
-                <h2>Name</h2>
-                <p>name@email.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
---
+++ src/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    
+    const cardsArray = robots.map(robot => {
+        return (<Card key={robot.name + robot.id} id={robot.id} name={robot.name} email={robot.email} />);
+    });
+
+    return (
+        <div>
+            {cardsArray}
+        </div>
+    );
+}
+
+export default CardList;
