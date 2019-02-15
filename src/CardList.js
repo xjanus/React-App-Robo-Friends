f50d0d3bccbb7bc821220d5606be0860a832e712
xjanus <xjanus.official@gmail.com>
2019-02-15 13:57:27 +0200

    heippaaidoih

--- src/CardList.js
+++ src/CardList.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    
-    const cardsArray = robots.map(robot => {
-        return (<Card key={robot.name + robot.id} id={robot.id} name={robot.name} email={robot.email} />);
-    });
 
     return (
         <div>
-            {cardsArray}
+        {robots.map(robot => {
+            return (<Card 
+                    key={robot.name + robot.id} 
+                    id={robot.id} name={robot.name} 
+                    email={robot.email} 
+                    />
+                    );
+            })
+            }
         </div>
     );
 }
