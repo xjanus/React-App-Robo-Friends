f02828789b8ecacf1854ac63c31db6713f50c4ee
xjanus <xjanus.official@gmail.com>
2019-02-18 10:16:05 +0200

    lookingchokiiiirrrtttt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
+import './App.css'
 
 class App extends Component {
     
@@ -30,7 +31,7 @@ class App extends Component {
         return (
             <div className='tc'>
                 
-                <h1>RoboFriends</h1>
+                <h1 className='f2'>RoboFriends</h1>
                 
                 <SearchBox searchfield={this.state.searchfield} searchChange={this.onSearchChange} />
                 
