b86be49311d1def49d7851cc9d44cf282d7d5955
xjanus <xjanus.official@gmail.com>
2019-02-18 11:17:01 +0200

    dependent

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
             })
             .then(users => {
                 this.setState({robots: users});
-            })        
+            });        
     }
 
     render() {
