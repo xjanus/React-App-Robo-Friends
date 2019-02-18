cc1cd166879833fbf549c7bf190590207acd45c3
xjanus <xjanus.official@gmail.com>
2019-02-18 12:10:24 +0200

    package.json

--- src/App.js
+++ src/App.js
@@ -28,19 +28,18 @@ class App extends Component {
     }
 
     render() {
+        const {robots, searchfield} = this.state;
 
         //filteredRobots array has only those robot objects whose name matches the search 
         //field state.
         //The filter() method creates a new array with all elements that pass the test implemented 
         //by the provided function. 
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if(this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ? <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     
                     <h1 className='f1'>RoboFriends</h1>
@@ -54,7 +53,6 @@ class App extends Component {
 
                 </div>        
             );
-        }
     }   
 }
 
