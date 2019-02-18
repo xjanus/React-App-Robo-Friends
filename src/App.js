ef919186ccd9ee5c840218f9e8c549df35e6ca15
xjanus <xjanus.official@gmail.com>
2019-02-18 11:15:59 +0200

    dependent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css'
 
 class App extends Component {
@@ -9,13 +8,23 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:''
         }
     }
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value});
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                response.json();
+            })
+            .then(users => {
+                this.setState({robots: users});
+            })        
     }
 
     render() {
